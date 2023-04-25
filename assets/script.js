fcc4f1ee0f09c0d6941f3e959decca52282c0739
neto1895 <cruz.rosales.ernesto@gmail.com>
2023-04-24 23:18:37 -0600

    added comments to js code

--- assets/script.js
+++ assets/script.js
@@ -5,27 +5,32 @@ var latitude;
 var longitude;
 // Function to retrieve the latitude and longitude of a city using the OpenWeatherMap Geocoding API
 function geoCODE_call(queryURL){
-        // Construct the API query URL with the specified city and API key
+    // Construct the API query URL with the specified city and API key
     var queryURL ="https://api.openweathermap.org/geo/1.0/direct?q="+city+"&appid="+APIkey;
-    fetch(queryURL)     // Fetch the data from the API using the query URL
+    fetch(queryURL)     
+    // Fetch the data from the API using the query URL
         .then(function (response) {
             return response.json();
         })
-        .then(function(data) { // Extract the latitude and longitude from the API response data
+        .then(function(data) { 
+            // Extract the latitude and longitude from the API response data
             city = data[0].name;
             latitude = data[0].lat;
             longitude = data[0].lon;
-        })        // Call the functions to retrieve and display the current weather data and 5-day forecast
+        })        
+        // Call the functions to retrieve and display the current weather data and 5-day forecast
         .then(secondAPIcal)
         .then(fiveDaysForecast)
     }
-    function secondAPIcal(){ // Function to retrieve and display the current weather data
+    function secondAPIcal(){ 
+        // Function to retrieve and display the current weather data
         var QueryURL_current = "https://api.openweathermap.org/data/2.5/weather?lat="+latitude+"&lon="+longitude + "&appid=" + APIkey + "&units=metric";
         fetch(QueryURL_current)
             .then(function(secondResponse) {
                 return  secondResponse.json();
             })
-            .then(function(data2){         // Display the retrieved weather data on the dashboard
+            .then(function(data2){         
+                // Display the retrieved weather data on the dashboard
                     document.querySelector(".city_pic").src = "https://source.unsplash.com/random/1600x900/?city%20"+city;
                     document.querySelector(".current_city_name").textContent = city;
                     document.querySelector(".date_"+0).textContent = new Date();
@@ -35,14 +40,16 @@ function geoCODE_call(queryURL){
                     document.querySelector(".Wind_"+0).textContent = "Wind: "+data2.wind.speed+"  m/sec";
                     document.querySelector(".Humidity_"+0).textContent = "Humidity: "+data2.main.humidity+"  %";
             })
-    }// Function to retrieve and display the 5-day forecast
+    }
+    // Function to retrieve and display the 5-day forecast
     function fiveDaysForecast(){
         var QueryURL_5 = "https://api.openweathermap.org/data/2.5/forecast?lat="+latitude+"&lon="+longitude + "&appid=" + APIkey + "&units=metric";
         fetch(QueryURL_5)
             .then(function(secondResponse) {
                 return  secondResponse.json();
             })
-            .then(function(data3){        // Display the retrieved weather data on the dashboard
+            .then(function(data3){        
+                // Display the retrieved weather data on the dashboard
                 for (var i = 3; i < data3.list.length; i ++){
                     document.querySelector(".current_city_name").textContent = city;
                     document.querySelector(".date_"+i).textContent = data3.list[i].dt_txt;
@@ -84,5 +91,8 @@ function init(){
     createBtn();
     geoCODE_call();
 }
+// calls function when page loads
 init();
+
+// selector for the search button
 document.querySelector(".submit_city").addEventListener("click",search_city);
